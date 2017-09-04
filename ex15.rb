b80def7c83ca8c86cbe9de421dacd9d791d3eb47
mjrdelgado <mjrdelgado@gmail.com>
2017-09-03 19:14:26 -0600

    Add ex15.rb

---
+++ ex15.rb
@@ -0,0 +1,20 @@
+# Argument for a file name
+filename = ARGV.first
+# Creates a file object
+txt = open(filename)
+
+puts "Here's your file #{filename}:"
+# Prints the contents of the file object
+print txt.read
+# Close file when we are done with them
+txt.close()
+
+print "Type the filename again: "
+# Gets file name by console input
+file_again = $stdin.gets.chomp
+# Creates a file object
+txt_again = open(file_again)
+# Prints the contents of the file object
+print txt_again.read
+# Close file when we are done with them
+txt_again.close()

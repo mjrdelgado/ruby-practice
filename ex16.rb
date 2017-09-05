3ded21ac609726ce24e21b14d8a8843e515ac022
mjrdelgado <mjrdelgado@gmail.com>
2017-09-04 20:20:57 -0600

    Finish ex16.rb. Add Study drills

--- ex16.rb
+++ ex16.rb
@@ -7,9 +7,13 @@ puts "If you do want that, hit RETURN."
 $stdin.gets
 
 puts "Opening the file..."
+# Remember, target is an object, not the file itself
 target = open(filename, 'w')
 
 puts "Truncating the file. Goodbye!"
+# Truncate deletes the contents of the file object
+# Since we opened the file with mode 'w', additional truncation is not needed
+# but we'll keep it for the skae of the exercise
 target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
@@ -23,12 +27,17 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("#{line1}\n#{line2}\n#{line3}\n")
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 puts "And finally, we close it."
+target.close
+
+target = open(filename)
+puts "This is what the file currently has: ", target.read
 target.close
